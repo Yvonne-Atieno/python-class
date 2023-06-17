b9ac6235bd8cf702e9109fb2045af1608bc92f2d
Yvonne-Atieno <emmanangida@gmail.com>
2023-06-17 16:10:56 +0300

    first commit

--- projects/greenkiosk/customer/admin.py
+++ projects/greenkiosk/customer/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .model import Product
+from .models import Product
 class productAdmin(admin.ModelAdmin):
     list_display=("name","stock","price","date_created")
 
     admin.site.register(Product,ProductAdmin)
+
+
+
+
+
+
